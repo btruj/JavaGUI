d57dcb7c54813a2a74c43107a7aeeab7532039ca
Bryan Trujillo <113075187+btruj@users.noreply.github.com>
2023-01-04 18:45:37 -0500

    Update BryanGUI.java
    Added Comments for better understanding of what code does.

--- MyFirstJavaGUI/src/BryanGUI.java
+++ MyFirstJavaGUI/src/BryanGUI.java
@@ -9,50 +9,50 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-// GUI stands for Graphical User Interface
 public class BryanGUI implements ActionListener {
+private int count = 0;
+private JLabel label; // displays the number of button clicks
+private JFrame frame; // container for the GUI elements
+private JPanel panel; // container for the button and label
+private JButton button; // button that the user clicks
 
-	private int count = 0;
-	private JLabel label;
-	private JFrame frame;
-	private JPanel panel;
-	private JButton button;
-	
-	public BryanGUI() {
-		
-		 frame = new JFrame();
-		
-		 button = new JButton("Click me");
-		
-		button.addActionListener(this);
-		
-		label = new JLabel("Number of clicks: 0");
-		
-		 panel = new JPanel();
-		panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
-		panel.setLayout(new GridLayout(0, 1));
-		panel.add(button);
-		panel.add(label);
-	
-		frame.add(panel, BorderLayout.CENTER);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setTitle("BT 1st GUI");
-		frame.pack();
-		frame.setVisible(true);
-		
-	
-	}
-	
-	public static void main(String[] args) {
-		new BryanGUI();
+public BryanGUI() {
+	// initialize the frame and set its properties
+	frame = new JFrame();
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.setTitle("BT 1st GUI");
 
-	}
+	// initialize the button and add an action listener to it
+	button = new JButton("Click me");
+	button.addActionListener(this);
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		count++;
-		label.setText("Number of clicks: " + count);
-	}
+	// initialize the label and set its initial text
+	label = new JLabel("Number of clicks: 0");
 
+	// initialize the panel and set its layout and border
+	panel = new JPanel();
+	panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+	panel.setLayout(new GridLayout(0, 1));
+
+	// add the button and label to the panel
+	panel.add(button);
+	panel.add(label);
+
+	// add the panel to the frame and set its size and visibility
+	frame.add(panel, BorderLayout.CENTER);
+	frame.pack();
+	frame.setVisible(true);
+}
+
+public static void main(String[] args) {
+	// create a new instance of the BryanGUI class
+	new BryanGUI();
+}
+
+@Override
+public void actionPerformed(ActionEvent e) {
+	// increment the count and update the label text when the button is clicked
+	count++;
+	label.setText("Number of clicks: " + count);
+}
 }
